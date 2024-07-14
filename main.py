85c3ff8c4cf09c13f0f2a2dca72e3b3256b4b693
Jonathan Zamora <jonzamora18@gmail.com>
2024-07-14 05:14:40 -0700

    add video and episode stats wrappers

--- main.py
+++ main.py
@@ -1,17 +1,23 @@
 import gymnasium as gym
 import gymnasium_env
-from gymnasium.wrappers import FlattenObservation
+from gymnasium.wrappers import FlattenObservation, RecordEpisodeStatistics, RecordVideo
 
-env = gym.make("gymnasium_env/GridWorld-v0", size=10, render_mode="human")
+num_eval_episodes = 4
+env = gym.make("gymnasium_env/GridWorld-v0", size=10, render_mode="rgb_array")
 env = FlattenObservation(env)
-observation, info = env.reset(seed=42)
+env = RecordVideo(env, video_folder="videos", episode_trigger=lambda x: True)
+env = RecordEpisodeStatistics(env, deque_size=num_eval_episodes)
 
-for step in range(300):
-    action = env.action_space.sample()  # this is where you would insert your policy
-    observation, reward, terminated, truncated, info = env.step(action)
-    print(observation)
+for episode_num, seed in zip(range(num_eval_episodes), range(42, 46)):
+    observation, info = env.reset(seed=seed)
+    episode_over = False
 
-    if terminated or truncated:
-        observation, info = env.reset()
+    while not episode_over:
+        action = env.action_space.sample()  # this is where you would insert your policy
+        observation, reward, terminated, truncated, info = env.step(action)
+        episode_over = terminated or truncated
 
 env.close()
+
+print(f"Episode Total Rewards: {env.return_queue}")
+print(f"Episode Lengths: {env.length_queue}")
